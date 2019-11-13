2fc14197a02f25b8e9a27acc302e057faa9e922e
sunnym97 <15201030sunny@viva-technology.org>
2019-11-13 18:09:50 +0530

    Gambler if won or lost
    50% of the stake,
    would resign for the
    day

--- GamblerStimulator.sh
+++ GamblerStimulator.sh
@@ -5,18 +5,30 @@ stake=0
 value=0
 resignVal=0
 val=0
+win=0
+lost=0
 #INPUTS
 read -p "Enter the number of days : " noOfDays
 read -p "Enter the number of Games : " noOfGames
 read -p "Enter the % to resign for the day " resignValue
 
-function resign()
+function resignLow()
 {
-	resignVal=$(( ($resignValue*$stake)/100 ))
-	Val=$(( $resignVal + $stake ))
-	echo $Val
-	return $Val
+	resignVal1=$(( ($resignValue*$stake)/100 ))
+	Val1=$(( $resignVal1 + $stake ))
+	echo $Val1
+	return $Val1
 }
+
+function resignHigh()
+{
+	resignVal2=$(( ($resignValue*$stake)/100 ))
+        Val2=$(( $stake - $resignVal2 ))
+        echo $Val2
+        return $Val2
+
+}
+
 for (( i=1; i<=$noOfDays; i++ ))
 do
 	stake=$(( $stake +100 ))
@@ -25,9 +37,9 @@ do
 		bet=$(( $bet +1 ))
 		winLose=$(( RANDOM%2 ))
 		echo $winLose
-		value="$( resign )"
-		echo $value
-		if [ $value -gt $stake ]
+		value1="$( resignLow )"
+		value2="$( resignHigh )"
+		if [ $value1 -gt $stake ] && [ $value2 -lt $stake ]
 		then
 			echo "He resigns for the day"
 			break
